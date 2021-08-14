8404816227fe37d179db37514b64fdaac9244907
Gustavo Ribeiro <gustavo.mribeiroxg@yahoo.com>
2021-08-14 15:46:30 -0300

    Package controller e model

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,15 +1,17 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.model.CustomerModel
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("customer")
 class CustomerController {
 
     @GetMapping
-
-    fun helloworld(): String{
-        return "Hello World"
+    fun getCustomer(): CustomerModel{
+        return CustomerModel("1", "Gustavo", "email@email.com")
     }
 
 }
---
+++ src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
@@ -0,0 +1,10 @@
+package com.mercadolivro.model
+
+data class CustomerModel (
+    var id: String,
+
+    var name: String,
+
+    var email: String
+
+)
