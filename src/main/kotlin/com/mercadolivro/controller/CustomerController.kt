e0c4bb4b273871818170b4dbab38e35a768b5890
Gustavo Ribeiro <gustavo.mribeiroxg@yahoo.com>
2021-08-18 01:59:04 -0300

    Concluding Controller Classes

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -11,7 +11,10 @@ class CustomerController {
     val customers= mutableListOf<CustomerModel>()
 
     @GetMapping
-    fun getCustomer(): MutableList<CustomerModel> {
+    fun getAll(@RequestParam name: String?): List<CustomerModel> {
+        name?.let {
+            return customers.filter { it.name.contains(name, true) }
+        }
         return customers
     }
 
@@ -26,4 +29,23 @@ class CustomerController {
         customers.add(CustomerModel(id, customer.name, customer.email))
     }
 
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: String): CustomerModel {
+        return customers.filter { it.id == id }.first()
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: String, @RequestBody customer: PostCustomerRequest) {
+        customers.filter { it.id == id }.first().let {
+            it.name= customer.name
+            it.email= customer.email
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: String) {
+        customers.removeIf { it.id== id }
+    }
 }
---
+++ src/main/kotlin/com/mercadolivro/request/PutCustomerRequest.kt
@@ -0,0 +1,7 @@
+package com.mercadolivro.request
+
+data class PutCustomerRequest (
+    var name: String,
+
+    var email: String
+)
