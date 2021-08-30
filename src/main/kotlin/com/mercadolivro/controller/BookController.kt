47f021b51f4fa7e8e8b97da559be4819b744fa42
Gustavo Ribeiro <gustavo.mribeiroxg@yahoo.com>
2021-08-30 19:41:01 -0300

    Creating new methods of Book

--- src/main/kotlin/com/mercadolivro/controller/BookController.kt
+++ src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -1,13 +1,13 @@
 package com.mercadolivro.controller
 
 import com.mercadolivro.controller.request.PostBookRequest
+import com.mercadolivro.controller.request.PutBookRequest
 import com.mercadolivro.extension.toBookModel
+import com.mercadolivro.model.BookModel
 import com.mercadolivro.service.BookService
 import com.mercadolivro.service.CustomerService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("book")
@@ -17,8 +17,37 @@ class BookController (
 ){
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody request: PostBookRequest) {
         val customer = customerService.getById(request.customerId)
         bookService.create(request.toBookModel(customer))
+    }
+
+    @GetMapping
+    fun findAll(): List<BookModel>{
+        return bookService.findAll()
+    }
+
+    @GetMapping("/active")
+    fun findActives(): List<BookModel>{
+        return bookService.findActives()
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Int): BookModel{
+        return bookService.findById(id)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: Int){
+        bookService.delete(id)
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: Int, @RequestBody book: PutBookRequest){
+        val bookSaved= bookService.findById(id)
+        bookService.update(book.toBookModel(bookSaved))
     }
 }
