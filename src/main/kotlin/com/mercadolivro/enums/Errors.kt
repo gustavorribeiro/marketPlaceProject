520c8c2766769c712a8f3f235996c4b5b1c7e81c
Gustavo Ribeiro <gustavo.mribeiroxg@yahoo.com>
2021-09-01 13:07:03 -0300

    Processing exception

--- src/main/kotlin/com/mercadolivro/enums/Errors.kt
+++ src/main/kotlin/com/mercadolivro/enums/Errors.kt
@@ -1,7 +1,11 @@
 package com.mercadolivro.enums
 
 enum class Errors (val code: String, val message: String) {
+
+    ML0001("ML-0001", "Invalid request"),
+
     ML1001("ML-1001", "Book id= %s not exists"),
     ML1002("ML-1002", "Cannot update book with status %s"),
+
     ML2001("ML-2001", "Customer id= %s not exists")
 }
--- src/main/kotlin/com/mercadolivro/exception/ControllerAdvice.kt
+++ src/main/kotlin/com/mercadolivro/exception/ControllerAdvice.kt
@@ -1,8 +1,11 @@
 package com.mercadolivro.exception
 
 import com.mercadolivro.controller.response.ErrorResponse
+import com.mercadolivro.controller.response.FieldErrorResponse
+import com.mercadolivro.enums.Errors
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.context.request.WebRequest
@@ -30,5 +33,16 @@ class ControllerAdvice {
             null
         )
         return ResponseEntity(erro, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(ex: MethodArgumentNotValidException, request: WebRequest): ResponseEntity<ErrorResponse> {
+        val erro= ErrorResponse(
+            HttpStatus.UNPROCESSABLE_ENTITY.value(),
+            ex.message,
+            Errors.ML0001.code,
+            ex.bindingResult.fieldErrors.map { FieldErrorResponse(it.defaultMessage ?: "Invalid", it.field) }
+        )
+        return ResponseEntity(erro, HttpStatus.UNPROCESSABLE_ENTITY)
     }
 }
